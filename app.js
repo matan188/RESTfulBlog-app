8257055c9bccba93e5ef91641e3800530236839d
Matan <matan.modai@gmail.com>
2017-09-08 09:26:09 +0300

    Added NEW and CREATE routes

--- app.js
+++ app.js
@@ -28,13 +28,29 @@ app.get('/', (req, res) => {
     res.redirect('/blogs');
 })
 
-// INDEX 
+// INDEX route
 app.get('/blogs', (req, res) => {
     Blog.find({}, (err, blogs) => {
         if (err) {
             console.log(err);
         } else {
             res.render('index', {blogs});
+        }
+    });
+})
+
+// NEW route
+app.get('/blogs/new', (req, res) => {
+    res.render('new');
+})
+
+// CREATE route
+app.post('/blogs', (req, res) => {
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if (err) {
+            res.render('new');
+        } else {
+            res.redirect('/blogs');
         }
     });
 })
